d1488f81214510dd08f63c79e0ae4e24b9fa9958
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-09-19 15:20:53 +0700

    using class as interface

--- interface.js
+++ interface.js
@@ -1,23 +1,250 @@
-function printLabel(labelledObj) {
-    console.log(labelledObj.label);
-}
-var myObj = { size: 10, label: "Size 10 Object" };
-printLabel(myObj);
-function createSquare(config) {
-    var newSqure = { color: "white", area: 100 };
-    if (config.color) {
-        newSqure.color = config.color;
-    }
-    if (config.width) {
-        newSqure.area = config.width * config.width;
-    }
-    return newSqure;
-}
-var mySqure = createSquare({ color: "blank" });
-var mySearch;
-mySearch = function (source, subString) {
-    var result = source.search(subString);
-    if (result == -1)
-        return false;
-    return true;
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
+var firstInterface;
+(function (firstInterface) {
+    function printLabel(labelledObj) {
+        console.log(labelledObj.label);
+    }
+    var myObj = { size: 10, label: "Size 10 Object" };
+    printLabel(myObj);
+    function createSquare(config) {
+        var newSqure = { color: "white", area: 100 };
+        if (config.color) {
+            newSqure.color = config.color;
+        }
+        if (config.width) {
+            newSqure.area = config.width * config.width;
+        }
+        return newSqure;
+    }
+    var mySqure = createSquare({ color: "blank" });
+})(firstInterface || (firstInterface = {}));
+var functionType;
+(function (functionType) {
+    var mySearch;
+    mySearch = function (source, subString) {
+        var result = source.search(subString);
+        if (result == -1)
+            return false;
+        return true;
+    };
+})(functionType || (functionType = {}));
+var ArrayType;
+(function (ArrayType) {
+    var myArray = ["Bob", "Fred"];
+})(ArrayType || (ArrayType = {}));
+var classType;
+(function (classType) {
+    var Clock = (function () {
+        function Clock(h, n) {
+        }
+        return Clock;
+    })();
+    var Clock3 = (function () {
+        function Clock3(h, m) {
+        }
+        return Clock3;
+    })();
+    var cs = Clock3;
+    var newClick = new cs(10, 10);
+})(classType || (classType = {}));
+var extendInterface;
+(function (extendInterface) {
+    var square = {};
+    square.color = "blue";
+    square.sideLength = 10;
+    var square3 = {};
+    square3.color = "blue";
+    square3.sideLength = 50;
+    square3.pedWidth = 50;
+})(extendInterface || (extendInterface = {}));
+var hybridType;
+(function (hybridType) {
+    var c;
+    c(10);
+})(hybridType || (hybridType = {}));
+var classes;
+(function (classes) {
+    var Greeter = (function () {
+        function Greeter(message) {
+            this.greeting = message;
+        }
+        Greeter.prototype.greet = function () {
+            return "Hello, " + this.greeting;
+        };
+        return Greeter;
+    })();
+    var geeter = new Greeter("Hello");
+})(classes || (classes = {}));
+var interitance;
+(function (interitance) {
+    var Animal = (function () {
+        function Animal(theName) {
+            this.name = theName;
+        }
+        Animal.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 0; }
+            alert(this.name + " moved" + meters + "m.");
+        };
+        return Animal;
+    })();
+    var Snake = (function (_super) {
+        __extends(Snake, _super);
+        function Snake(name) {
+            _super.call(this, name);
+        }
+        Snake.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 5; }
+            alert("slithering...");
+            _super.prototype.move.call(this, meters);
+        };
+        return Snake;
+    })(Animal);
+    var Horse = (function (_super) {
+        __extends(Horse, _super);
+        function Horse(name) {
+            _super.call(this, name);
+        }
+        Horse.prototype.move = function (meters) {
+            if (meters === void 0) { meters = 45; }
+            alert("Gallooping");
+            _super.prototype.move.call(this, meters);
+        };
+        return Horse;
+    })(Animal);
+    var sam = new Snake("Summy the Python");
+    var tom = new Horse("Tommy the Palomino");
+    sam.move();
+    tom.move(35);
+})(interitance || (interitance = {}));
+var publicByDefault;
+(function (publicByDefault) {
+    var Animal = (function () {
+        function Animal(theName) {
+            this.name = theName;
+        }
+        Animal.prototype.meove = function (meters) {
+            alert(this.name + " moved " + meters + "m.");
+        };
+        return Animal;
+    })();
+})(publicByDefault || (publicByDefault = {}));
+var underStandPrivate;
+(function (underStandPrivate) {
+    var Animal = (function () {
+        function Animal(theName) {
+            this.name = theName;
+        }
+        return Animal;
+    })();
+    var Rhino = (function (_super) {
+        __extends(Rhino, _super);
+        function Rhino() {
+            _super.call(this, "Rhino");
+        }
+        return Rhino;
+    })(Animal);
+    var Employee = (function () {
+        function Employee(theName) {
+            this.name = theName;
+        }
+        return Employee;
+    })();
+    var animal = new Animal("Goat");
+    var rhino = new Rhino();
+    var employee = new Employee("Bob");
+    animal = rhino;
+})(underStandPrivate || (underStandPrivate = {}));
+var parameterProperty;
+(function (parameterProperty) {
+    var Animal = (function () {
+        function Animal(name) {
+            this.name = name;
+        }
+        Animal.prototype.move = function (meters) {
+            alert(this.name + "moved " + meters + "m.");
+        };
+        return Animal;
+    })();
+})(parameterProperty || (parameterProperty = {}));
+var accessor;
+(function (accessor) {
+    var Employee = (function () {
+        function Employee() {
+        }
+        return Employee;
+    })();
+    var employee = new Employee();
+    employee.fullName = "Bob Smith";
+    if (employee.fullName) {
+        alert(employee.fullName);
+    }
+})(accessor || (accessor = {}));
+var setget;
+(function (setget) {
+    var passcode = "secret passcode";
+    var Employee = (function () {
+        function Employee() {
+        }
+        Object.defineProperty(Employee.prototype, "fullName", {
+            get: function () { return this._fullName; },
+            set: function (newName) {
+                if (passcode && passcode == "secret passcode") {
+                    this._fullName = newName;
+                }
+                else {
+                    alert("error");
+                }
+            },
+            enumerable: true,
+            configurable: true
+        });
+        return Employee;
+    })();
+})(setget || (setget = {}));
+var staticProperties;
+(function (staticProperties) {
+    var Grid = (function () {
+        function Grid(scale) {
+            this.scale = scale;
+        }
+        Grid.prototype.calculateDistanceFromOrigin = function (point) {
+            var xDist = (point.x - Grid.origin.x);
+            var yDist = (point.y - Grid.origin.y);
+            return Math.sqrt(xDist * yDist + yDist * yDist) / this.scale;
+        };
+        Grid.origin = { x: 0, y: 0 };
+        return Grid;
+    })();
+    var grid1 = new Grid(1.0);
+    var grid2 = new Grid(5.0);
+    alert(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+    alert(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+})(staticProperties || (staticProperties = {}));
+var advancedTechnique;
+(function (advancedTechnique) {
+    var Greeter = (function () {
+        function Greeter(message) {
+            this.greeting = message;
+        }
+        Greeter.prototype.greet = function () {
+            return "hello, " + this.greeting;
+        };
+        return Greeter;
+    })();
+    var greeter;
+    greeter = new Greeter("world");
+})(advancedTechnique || (advancedTechnique = {}));
+var useingClassAsAnInterface;
+(function (useingClassAsAnInterface) {
+    var Point = (function () {
+        function Point() {
+        }
+        return Point;
+    })();
+    var point3d = { x: 1, y: 2, z: 3 };
+})(useingClassAsAnInterface || (useingClassAsAnInterface = {}));
+var end = "end";
--- interface.ts
+++ interface.ts
@@ -1,42 +1,327 @@
 
 
-function printLabel(labelledObj: { label: string} ) {
-    console.log(labelledObj.label);
-}
+module firstInterface {
 
-interface LabbedValue {
-    label: string;
-}
-
-var myObj = { size: 10, label: "Size 10 Object"};
-printLabel(myObj);
-
-
-interface SqureConfig {
-    color?: string;
-    width?: number;
-}
-
-function createSquare(config: SqureConfig): { color: string, area: number } {
-    var newSqure ={ color: "white", area: 100 };
-    if(config.color) {
-        newSqure.color = config.color;
+    function printLabel(labelledObj: { label: string }) {
+        console.log(labelledObj.label);
     }
-    if(config.width) {
-        newSqure.area = config.width * config.width;
+
+    interface LabbedValue {
+        label: string;
     }
-    return newSqure;
+
+    var myObj = { size: 10, label: "Size 10 Object" };
+    printLabel(myObj);
+
+
+    interface SqureConfig {
+        color?: string;
+        width?: number;
+    }
+
+    function createSquare(config: SqureConfig):
+        { color: string, area: number } {
+        var newSqure = { color: "white", area: 100 };
+        if (config.color) {
+            newSqure.color = config.color;
+        }
+        if (config.width) {
+            newSqure.area = config.width * config.width;
+        }
+        return newSqure;
+    }
+
+    var mySqure = createSquare({ color: "blank" });
 }
 
-var mySqure = createSquare({ color: "blank"});
+// function type
+module functionType {
+    interface SearchFunc {
+        (source: string, subString: string): boolean;
+    }
 
-interface SearchFunc {
-    (source: string, subString: string): boolean;
-}
-
-var mySearch: SearchFunc;
-mySearch = function(source: string, subString: string) {
+    var mySearch: SearchFunc;
+    mySearch = function(source: string, subString: string) {
         var result = source.search(subString);
-        if(result == -1) return false;
+        if (result == -1) return false;
         return true;
+    }
 }
+
+// array types
+module ArrayType {
+    interface StringArray {
+        [index: number]: string
+    }
+
+    var myArray: StringArray = ["Bob", "Fred"];
+
+    interface Dictionary {
+        [index: string]: string;
+        //length: number;
+    }
+}
+
+
+// class type
+module classType {
+    interface ClockInterface {
+        currentTime: Date;
+    }
+
+    class Clock implements ClockInterface {
+        currentTime: Date;
+        constructor(h: number, n: number) { }
+    }
+
+    // difference bettween static/instatnce side of class
+    module diffence {
+
+        interface ClockInterface2 {
+            new (hour: number, minute: number);
+        }
+    }
+
+    /*
+    class Clock2 implements ClockInterface2 {
+        currentTime: Date;
+        constructor(h: number, m: number) {}
+    }*/
+
+    interface ClockStatic {
+        new (hour: number, minute: number);
+    }
+
+    class Clock3 {
+        currentTime: Date;
+        constructor(h: number, m: number) { }
+    }
+
+    var cs: ClockStatic = Clock3;
+    var newClick = new cs(10, 10);
+}
+
+// extending interface
+module extendInterface {
+    interface Shape {
+        color: string;
+    }
+
+    interface Square2 extends Shape {
+        sideLength: number;
+    }
+
+    var square = <Square2> {};
+    square.color = "blue";
+    square.sideLength = 10;
+
+    interface PendStroke {
+        pedWidth: number;
+    }
+
+    interface Square3 extends Shape, PendStroke {
+        sideLength: number;
+    }
+
+    var square3 = <Square3> {};
+    square3.color = "blue";
+    square3.sideLength = 50;
+    square3.pedWidth = 50;
+}
+
+// hybrid types
+module hybridType {
+    interface Couter {
+        (start: number): string;
+        interval: number;
+        reset(): void;
+    }
+    var c: Couter;
+    c(10);
+}
+
+
+module classes {
+
+    class Greeter {
+        greeting: string;
+
+        constructor(message: string) {
+            this.greeting = message;
+        }
+
+        greet(): string {
+            return "Hello, " + this.greeting;
+        }
+    }
+
+    var geeter = new Greeter("Hello");
+}
+
+
+module interitance {
+    class Animal {
+        name: string;
+        constructor(theName: string) {
+            this.name = theName;
+        }
+        move(meters: number = 0) {
+            alert(this.name + " moved" + meters + "m.");
+        }
+    }
+
+    class Snake extends Animal {
+        constructor(name: string) { super(name); }
+        move(meters = 5) {
+            alert("slithering...");
+            super.move(meters);
+        }
+    }
+
+    class Horse extends Animal {
+        constructor(name: string) { super(name); }
+        move(meters = 45) {
+            alert("Gallooping");
+            super.move(meters);
+        }
+    }
+
+    var sam = new Snake("Summy the Python");
+    var tom: Animal = new Horse("Tommy the Palomino");
+    sam.move()
+    tom.move(35);
+}
+
+
+module publicByDefault {
+    class Animal {
+        private name: string;
+        constructor(theName: string) { this.name = theName; }
+        meove(meters: number) {
+            alert(this.name + " moved " + meters + "m.");
+        }
+    }
+}
+
+module underStandPrivate {
+    class Animal {
+        private name: string;
+        constructor(theName: string) { this.name = theName; }
+    }
+
+    class Rhino extends Animal {
+        constructor() { super("Rhino"); }
+    }
+
+    class Employee {
+        private name:  string;
+        constructor(theName: string) { this.name = theName; }
+    }
+
+    var animal = new Animal("Goat");
+    var rhino = new Rhino();
+    var employee = new Employee("Bob");
+
+    animal = rhino;
+    // animal = employee;
+}
+
+module parameterProperty {
+    class Animal {
+        constructor(private name: string) {
+
+        }
+        move(meters: number) {
+            alert(this.name + "moved " + meters + "m.");
+        }
+    }
+}
+
+module accessor {
+    class Employee {
+        fullName: string;
+    }
+
+    var employee = new Employee();
+    employee.fullName = "Bob Smith";
+    if(employee.fullName) {
+        alert(employee.fullName);
+    }
+}
+
+module setget {
+    var passcode = "secret passcode";
+
+    class Employee {
+        private _fullName: string;
+        get fullName(): string { return this._fullName; }
+        set fullName(newName: string) {
+            if(passcode && passcode == "secret passcode") {
+                this._fullName = newName;
+            }else {
+                alert("error");
+            }
+        }
+    }
+}
+
+module staticProperties {
+    class Grid {
+        static origin = { x: 0, y: 0};
+        calculateDistanceFromOrigin(point: { x: number, y: number}) {
+            var xDist = (point.x - Grid.origin.x);
+            var yDist = (point.y - Grid.origin.y);
+            return Math.sqrt(xDist * yDist + yDist * yDist) / this.scale;
+        }
+
+        constructor(public scale: number) {}
+    }
+
+    var grid1 = new Grid(1.0);
+    var grid2 = new Grid(5.0);
+
+    alert(grid1.calculateDistanceFromOrigin({x:10, y: 10}));
+    alert(grid1.calculateDistanceFromOrigin({x:10, y: 10}));
+}
+
+module advancedTechnique {
+    class Greeter {
+        greeting: string;
+        constructor(message: string) {
+            this.greeting = message;
+        }
+        greet() {
+            return "hello, " + this.greeting;
+        }
+    }
+
+    var greeter: Greeter;
+    greeter = new Greeter("world");
+}
+
+module useingClassAsAnInterface {
+    class Point {
+        x: number;
+        y: number;
+    }
+
+    interface Point3d extends Point {
+        z: number;
+    }
+    var point3d: Point3d = { x: 1, y:2, z: 3};
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var end = "end";
